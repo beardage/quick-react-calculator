bd35ef65bd7b43a02c44cc8f8be35be21b0821c6
Kyle Beard <xypress@gmail.com>
2022-08-17 14:29:03 -0700

    feature: adding hihello contact card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,9 +1,18 @@
 const ContactCard = () => {
   return (
-    <>
-      <p>Kyle Beard</p>
-      <a href="https://linkedin.com/in/kbeardage">Linkedin</a>
-    </>
+    <a
+      href="https://hihello.me/p/cf5e0c31-bc28-4d89-b1af-8f2422fb27c8"
+      rel="noopener noreferrer"
+      target="_blank"
+      className="inline-block"
+    >
+      <img
+        alt="This is Kyle Beard's card. Their email is kb@kylebeard.com. Their phone number is +1 602 799 8904."
+        src="https://cdn.hihello.me/cards/cf5e0c31-bc28-4d89-b1af-8f2422fb27c8/signature_square.png?generated=1660771473046"
+        width="360"
+        className="inline-block min-h-[100px]"
+      />
+    </a>
   );
 };
 export default ContactCard;
