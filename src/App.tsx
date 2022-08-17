a6da6fc34d7f18669e4805a977d37843478a8931
Kyle Beard <xypress@gmail.com>
2022-08-17 14:33:59 -0700

    refactor: adjusting various colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import ContactCard from "./components/ContactCard";
 
 function App() {
   return (
-    <div className="App bg-gray-800">
+    <div className="App bg-gray-600">
       <header>
-        <h1 className="text-3xl font-bold mt-8">Quick React Calculator</h1>
+        <h1 className="text-3xl font-bold mt-8 text-amber-500">
+          Quick React Calculator
+        </h1>
       </header>
       <Calculator />
       <ContactCard />
