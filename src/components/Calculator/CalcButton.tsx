4d3409aa64cfbe10e64feefde2703212ecec0b89
Kyle Beard <xypress@gmail.com>
2022-08-23 09:58:34 -0700

    feature: adding check to prevent double operator input

--- src/components/Calculator/CalcButton.tsx
+++ src/components/Calculator/CalcButton.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { useDisplay } from "../../context/DisplayContext";
 import { evaluate } from "mathjs";
 
@@ -7,21 +7,38 @@ type CalcButtonParams = {
     className?: string;
     children: ReactNode;
 };
+const operators = [".", "+", "-", "/", "*"];
+
 const CalcButton = ({ value, className, children }: CalcButtonParams) => {
     const { displayValue, setDisplayValue } = useDisplay();
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleOnClick = () => {
-        if (value !== "evaluate" && value !== "clear") {
-            // TODO: check for last character being a non-int and swap instead of adding
-            // TODO: check for empty string and show error when non-int value
-            setDisplayValue(displayValue + value);
-        }
         if (value === "clear") {
             setDisplayValue("");
         }
         if (value === "evaluate") {
             // TODO: parse displayValue and setDisplayValue with evaluated value
             setDisplayValue(String(evaluate(displayValue)));
+        }
+        // when a non-number value submits, check for non-number values in the last postition of the displayValue <string>
+        if (operators.indexOf(value as string) > 0) {
+            const lastVal = displayValue.slice(-1);
+            if (operators.indexOf(lastVal) > 0) {
+                setErrorMessage("Error: Two operators in a row");
+                setShowError(true);
+                setTimeout(() => {
+                    setShowError(false);
+                    setErrorMessage("");
+                }, 5000);
+                return;
+            }
+        }
+        if (value !== "evaluate" && value !== "clear") {
+            // TODO: check for last character being a non-int and swap instead of adding
+            // TODO: check for empty string and show error when non-int value
+            setDisplayValue(displayValue + value);
         }
     };
 
